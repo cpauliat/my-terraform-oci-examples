d1d3a7dc41fd1f992a9be49850d1fd250d74e9b1
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2019-11-12 15:39:43 +0100

    update

--- 01_OCI_demo_vcn_OL7_win2016_instances/userdata/bootstrap_win2016.ps1
+++ 01_OCI_demo_vcn_OL7_win2016_instances/userdata/bootstrap_win2016.ps1
@@ -3,10 +3,10 @@
 # ---------- Post-provisioning Cloud-init script for Windows 2016
 
 # -- Set a password for opc user (replace temporary randomly generated password)
-net user opc "WELcome2018##"
+net user opc "OCIpwd#2019#"
 
 # -- Create new user account opc2 with Administrators privileges (may be needed if opc is locked)
-net user opc2 "WELcome2018##" /add
+net user opc2 "OCIpwd#2019#" /add
 net localgroup  administrators opc2 /add
 
 # -- Disable password expiry (42 days by default)
