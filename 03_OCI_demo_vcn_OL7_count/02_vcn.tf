b7b4e93a1915c9063208b304f8012c1c5a165e34
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2019-11-21 12:41:03 +0100

    update

--- 03_OCI_demo_vcn_OL7_count/02_vcn.tf
+++ 03_OCI_demo_vcn_OL7_count/02_vcn.tf
@@ -4,37 +4,37 @@ data "oci_identity_availability_domains" "ADs" {
 }
 
 # ------ Create a new VCN
-resource "oci_core_virtual_network" "tf-demo06-vcn" {
+resource "oci_core_virtual_network" "tf-demo03-vcn" {
   cidr_block     = var.cidr_vcn
   compartment_id = var.compartment_ocid
-  display_name   = "tf-demo06-vcn"
+  display_name   = "tf-demo03-vcn"
   dns_label      = "tfdemovcn"
 }
 
 # ------ Create a new Internet Gategay
-resource "oci_core_internet_gateway" "tf-demo06-ig" {
+resource "oci_core_internet_gateway" "tf-demo03-ig" {
   compartment_id = var.compartment_ocid
-  display_name   = "tf-demo06-internet-gateway"
-  vcn_id         = oci_core_virtual_network.tf-demo06-vcn.id
+  display_name   = "tf-demo03-internet-gateway"
+  vcn_id         = oci_core_virtual_network.tf-demo03-vcn.id
 }
 
 # ------ Create a new Route Table
-resource "oci_core_route_table" "tf-demo06-rt" {
+resource "oci_core_route_table" "tf-demo03-rt" {
   compartment_id = var.compartment_ocid
-  vcn_id         = oci_core_virtual_network.tf-demo06-vcn.id
-  display_name   = "tf-demo06-route-table"
+  vcn_id         = oci_core_virtual_network.tf-demo03-vcn.id
+  display_name   = "tf-demo03-route-table"
 
   route_rules {
     destination       = "0.0.0.0/0"
-    network_entity_id = oci_core_internet_gateway.tf-demo06-ig.id
+    network_entity_id = oci_core_internet_gateway.tf-demo03-ig.id
   }
 }
 
 # ------ Create a new security list to be used in the new subnet
-resource "oci_core_security_list" "tf-demo06-subnet1-sl" {
+resource "oci_core_security_list" "tf-demo03-subnet1-sl" {
   compartment_id = var.compartment_ocid
-  display_name   = "tf-demo06-subnet1-security-list"
-  vcn_id         = oci_core_virtual_network.tf-demo06-vcn.id
+  display_name   = "tf-demo03-subnet1-security-list"
+  vcn_id         = oci_core_virtual_network.tf-demo03-vcn.id
 
   egress_security_rules {
     protocol    = "all"
@@ -76,16 +76,16 @@ resource "oci_core_security_list" "tf-demo06-subnet1-sl" {
 }
 
 # ------ Create a public subnet 1 in AD1 in the new VCN
-resource "oci_core_subnet" "tf-demo06-public-subnet1" {
+resource "oci_core_subnet" "tf-demo03-public-subnet1" {
 # uncomment the following line to create an AD specific subnet
 # availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1]["name"]
   cidr_block          = var.cidr_subnet1
-  display_name        = "tf-demo06-public-subnet1"
+  display_name        = "tf-demo03-public-subnet1"
   dns_label           = "subnet1"
   compartment_id      = var.compartment_ocid
-  vcn_id              = oci_core_virtual_network.tf-demo06-vcn.id
-  route_table_id      = oci_core_route_table.tf-demo06-rt.id
-  security_list_ids   = [oci_core_security_list.tf-demo06-subnet1-sl.id]
-  dhcp_options_id     = oci_core_virtual_network.tf-demo06-vcn.default_dhcp_options_id
+  vcn_id              = oci_core_virtual_network.tf-demo03-vcn.id
+  route_table_id      = oci_core_route_table.tf-demo03-rt.id
+  security_list_ids   = [oci_core_security_list.tf-demo03-subnet1-sl.id]
+  dhcp_options_id     = oci_core_virtual_network.tf-demo03-vcn.default_dhcp_options_id
 }
 
