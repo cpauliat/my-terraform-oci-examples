4cbc530c8d55c67ee70df21de880cd5e4182abe7
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2019-11-27 13:52:45 +0100

    update

--- 20_OCI_demo_autonomous_DB_serverless/02_adb.tf
+++ 20_OCI_demo_autonomous_DB_serverless/02_adb.tf
@@ -1,11 +1,22 @@
-resource "random_string" "autonomous_database_admin_password" {
-  length  = 16
-  special = true
+# ---- Generate a random string to be used as password for ADB admin user
+resource "random_string" "tf-demo20-adb-password" {
+  # must contains at least 2 upper case letters, 
+  # 2 lower case letters, 2 numbers and 2 special characters
+  length      = 16
+  upper       = true
+  min_upper   = 2
+  lower       = true
+  min_lower   = 2
+  number      = true
+  min_numeric = 2
+  special     = true
+  min_special = 2
+  override_special = "#+-="   # use only special characters in this list
 }
 
-resource "oci_database_autonomous_database" "autonomous_database" {
+resource "oci_database_autonomous_database" "tf-demo20-adb" {
   db_workload              = var.adb_type
-  admin_password           = random_string.autonomous_database_admin_password.result
+  admin_password           = random_string.tf-demo20-adb-password.result
   #admin_password           = var.adb_password
   compartment_id           = var.compartment_ocid
   cpu_core_count           = var.adb_cpu_core_count
@@ -22,9 +33,9 @@ resource "oci_database_autonomous_database" "autonomous_database" {
 output "ADB" {
   value = <<EOF
 
-  service console URL = ${oci_database_autonomous_database.autonomous_database.service_console_url}
+  service console URL = ${oci_database_autonomous_database.tf-demo20-adb.service_console_url}
             user      = admin
-            password  = ${random_string.autonomous_database_admin_password.result}
+            password  = ${random_string.tf-demo20-adb-password.result}
 EOF
 #            password  = ${var.adb_password}
 }
