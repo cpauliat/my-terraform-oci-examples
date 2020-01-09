f26cfb0f29ce31ecb0a24a4dee25b8a23525b5b5
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2020-01-09 16:46:49 +0100

    update

--- 03_OCI_demo_vcn_OL7_count/01_auth.tf
+++ 03_OCI_demo_vcn_OL7_count/01_auth.tf
@@ -6,13 +6,17 @@ variable "private_key_path" {}
 variable "compartment_ocid" {}
 variable "region" {}
 variable "AD" {}
-variable "BootStrapFile_ol7" {}
 variable "ssh_public_key_file_ol7" {}
 variable "ssh_private_key_file_ol7" {}
 variable "authorized_ips" {}
 variable "cidr_vcn" {}
 variable "cidr_subnet1" {}
 variable "number_of_instances" {}
+variable "instances_name" {}
+variable "instances_hostname" {}
+variable "instances_shape" {}
+variable "instances_post_prov" {}  
+
 
 # ---- provider
 provider "oci" {
--- 03_OCI_demo_vcn_OL7_count/03_instances_ol7.tf
+++ 03_OCI_demo_vcn_OL7_count/03_instances_ol7.tf
@@ -18,13 +18,14 @@ resource "oci_core_instance" "tf-demo03-ol7" {
 
   availability_domain  = data.oci_identity_availability_domains.ADs.availability_domains[var.AD - 1]["name"]
   compartment_id       = var.compartment_ocid
-  display_name         = "tf-demo03-ol7-${count.index}"
-  shape                = "VM.Standard2.1"
+  #display_name         = "tf-demo03-ol7-${count.index}"
+  display_name         = var.instances_name[count.index]
+  shape                = var.instances_shape[count.index]
   preserve_boot_volume = "false"
 
   create_vnic_details {
     subnet_id      = oci_core_subnet.tf-demo03-public-subnet1.id
-    hostname_label = "ol7-${count.index}"
+    hostname_label = var.instances_hostname[count.index]
   }
 
   source_details {
@@ -34,12 +35,8 @@ resource "oci_core_instance" "tf-demo03-ol7" {
 
   metadata = {
     ssh_authorized_keys = file(var.ssh_public_key_file_ol7)
-    user_data           = base64encode(file(var.BootStrapFile_ol7))
+    user_data           = base64encode(file(var.instances_post_prov[count.index]))
   }
-}
-
-output "public_IP_adresses_of_compute_instances" {
-  value = oci_core_instance.tf-demo03-ol7.*.public_ip
 }
 
 # ------ Display the complete ssh command needed to connect to the instances
@@ -47,37 +44,7 @@ output "public_IP_adresses_of_compute_instances" {
 output "SSH_connections" {
   value = [
     for instance in oci_core_instance.tf-demo03-ol7.*:
-      "ol7-${index(oci_core_instance.tf-demo03-ol7.*, instance)}: ssh -i ${var.ssh_private_key_file_ol7} opc@${instance.public_ip}"
+      "${var.instances_name[index(oci_core_instance.tf-demo03-ol7.*, instance)]}: ssh -i ${var.ssh_private_key_file_ol7} opc@${instance.public_ip}"
   ]
 
-}
-
-output "Instances" {
-  value = <<EOF
-
-
-  ---- You can SSH directly to the OL7 compute instances by typing the following ssh commands
-  for ol7-0: ssh -i ${var.ssh_private_key_file_ol7} opc@${oci_core_instance.tf-demo03-ol7[0].public_ip}
-  for ol7-1: ssh -i ${var.ssh_private_key_file_ol7} opc@${oci_core_instance.tf-demo03-ol7[1].public_ip}
-  for ol7-2: ssh -i ${var.ssh_private_key_file_ol7} opc@${oci_core_instance.tf-demo03-ol7[2].public_ip}
-
-  ---- Alternatively, you can add the following lines to your file $HOME/.ssh/config and then just run "ssh ol7-x"
-
-  Host ol7-0
-          Hostname ${oci_core_instance.tf-demo03-ol7[0].public_ip}
-          User opc
-          IdentityFile ${var.ssh_private_key_file_ol7}
-
-  Host ol7-1
-          Hostname ${oci_core_instance.tf-demo03-ol7[1].public_ip}
-          User opc
-          IdentityFile ${var.ssh_private_key_file_ol7}
-
-  Host ol7-2
-          Hostname ${oci_core_instance.tf-demo03-ol7[2].public_ip}
-          User opc
-          IdentityFile ${var.ssh_private_key_file_ol7}
-
-  
-EOF
 }
