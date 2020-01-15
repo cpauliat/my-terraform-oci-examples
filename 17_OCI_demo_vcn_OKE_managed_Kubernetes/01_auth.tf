3749e6752b624ebb22338ba35b5507ad3051c72c
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2020-01-15 09:33:14 +0100

    update

---
+++ 17_OCI_demo_vcn_OKE_managed_Kubernetes/01_auth.tf
@@ -0,0 +1,38 @@
+# ---- use variables defined in terraform.tfvars file
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "AD" {}
+variable "authorized_ips" {}
+variable "cidr_vcn" {}
+variable "cidr_worker" {}
+variable "cidr_lb" {}
+variable "cidrs_oke" {}
+variable "ssh_public_key_file" {}
+variable "ssh_private_key_file" {}
+variable "oke_cluster_name" {}
+variable "oke_cluster_k8s_version" {}
+variable "oke_cluster_k8s_dashboard_enabled" {}
+variable "oke_cluster_tiller_enabled" {}
+variable "oke_k8s_network_config_pods_cidr" {}
+variable "oke_k8s_network_config_services_cidr" {}
+variable "oke_node_pool_kubernetes_version" {}
+variable "oke_node_pool_name" {}
+variable "oke_node_pool_node_image_name" {}
+variable "oke_node_pool_node_shape" {}
+variable "oke_node_pool_initial_node_labels_key" {}
+variable "oke_node_pool_initial_node_labels_value" {}
+variable "oke_node_pool_nb_worker_nodes" {}
+
+# ---- provider
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
---
+++ 17_OCI_demo_vcn_OKE_managed_Kubernetes/02_vcn.tf
@@ -0,0 +1,175 @@
+# ------ Create a new VCN
+resource "oci_core_virtual_network" "tf-demo17-vcn" {
+  cidr_block     = var.cidr_vcn
+  compartment_id = var.compartment_ocid
+  display_name   = "tf-demo17-oke-vcn"
+  dns_label      = "demo17"
+}
+
+# ========== Common resources in the VCN
+
+# ------ Create a new Internet Gategay
+resource "oci_core_internet_gateway" "tf-demo17-ig" {
+  compartment_id = var.compartment_ocid
+  display_name   = "tf-demo17-internet-gateway"
+  vcn_id         = oci_core_virtual_network.tf-demo17-vcn.id
+}
+
+# ------ Create a new Route Table
+resource "oci_core_route_table" "tf-demo17-rt" {
+  compartment_id = var.compartment_ocid
+  vcn_id         = oci_core_virtual_network.tf-demo17-vcn.id
+  display_name   = "tf-demo17-route-table"
+
+  route_rules {
+    destination       = "0.0.0.0/0"
+    network_entity_id = oci_core_internet_gateway.tf-demo17-ig.id
+  }
+}
+
+# ============ WORKER NODES subnet and related resources
+
+# ------ Create a new security list for worker subnets
+resource "oci_core_security_list" "tf-demo17-sl-worker" {
+  compartment_id = var.compartment_ocid
+  display_name   = "tf-demo17-sl-worker"
+  vcn_id         = oci_core_virtual_network.tf-demo17-vcn.id
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"
+  }
+
+  ingress_security_rules {
+    protocol = "all"
+    source   = var.cidr_worker
+  }
+
+  # -- optional: allow SSH access to worker nodes
+  ingress_security_rules {
+    protocol = "6"
+    source   = var.authorized_ips
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+  
+  ingress_security_rules {
+    protocol = "1"   # icmp
+    source   = var.authorized_ips
+    icmp_options {
+      type = 3
+      code = 4
+    }
+  }
+
+  # -- optional: allow inbound traffic to worker nodes
+  ingress_security_rules {
+    protocol = "6"  # tcp
+    source   = var.authorized_ips
+    tcp_options {
+      min = 30000
+      max = 32767
+    }
+  }
+
+  # -- required: allow SSH access to worker nodes from OKE
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[0]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[1]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[2]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }
+
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[3]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }  
+
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[4]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }  
+
+  ingress_security_rules {
+    protocol = "6" # tcp
+    source   = var.cidrs_oke[5]
+    tcp_options {
+      min = 22
+      max = 22
+    }
+  }  
+}
+
+# ------ Create a regional public subnet for worker nodes
+# ------ (also possible to use a private network with NAT and services gateways)
+resource "oci_core_subnet" "tf-demo17-worker" {
+  cidr_block          = var.cidr_worker
+  display_name        = "tf-demo17-worker"
+  dns_label           = "worker"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_virtual_network.tf-demo17-vcn.id
+  route_table_id      = oci_core_route_table.tf-demo17-rt.id
+  security_list_ids   = [oci_core_security_list.tf-demo17-sl-worker.id]
+  dhcp_options_id     = oci_core_virtual_network.tf-demo17-vcn.default_dhcp_options_id
+}
+
+# ============ LOAD BALANCER subnet and related resources
+
+# ------ Create a new security list for load balancer subnet
+resource "oci_core_security_list" "tf-demo17-sl-loadbalancers" {
+  compartment_id = var.compartment_ocid
+  display_name   = "tf-demo17-sl-loadbalancers"
+  vcn_id         = oci_core_virtual_network.tf-demo17-vcn.id
+
+  egress_security_rules {
+    protocol    = "all"
+    destination = "0.0.0.0/0"   
+  }
+  
+  ingress_security_rules {
+    protocol = "all"
+    source   = "0.0.0.0/0"
+  }
+}
+
+# ------ Create a regional public subnet for load balancer
+resource "oci_core_subnet" "tf-demo17-lb" {
+  cidr_block          = var.cidr_lb
+  display_name        = "tf-demo17-lb"
+  dns_label           = "lb"
+  compartment_id      = var.compartment_ocid
+  vcn_id              = oci_core_virtual_network.tf-demo17-vcn.id
+  route_table_id      = oci_core_route_table.tf-demo17-rt.id
+  security_list_ids   = [oci_core_security_list.tf-demo17-sl-loadbalancers.id]
+  dhcp_options_id     = oci_core_virtual_network.tf-demo17-vcn.default_dhcp_options_id
+}
