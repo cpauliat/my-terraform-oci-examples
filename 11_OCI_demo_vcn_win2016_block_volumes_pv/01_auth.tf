9f9d89b734b7ee0b42eefb32a9a6fbf686775288
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2020-01-28 18:39:15 +0100

    update

---
+++ 11_OCI_demo_vcn_win2016_block_volumes_pv/01_auth.tf
@@ -0,0 +1,23 @@
+# ---- use variables defined in terraform.tfvars file
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "AD" {}
+variable "BootStrapFile_win2016" {}
+variable "authorized_ips" {}
+variable "cidr_vcn" {}
+variable "cidr_subnet1" {}
+variable "fs_label" {}
+
+# ---- provider
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
