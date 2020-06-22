e680ce2ee17ee962334dc302f2c7f63bcadd8791
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2020-06-22 16:11:41 +0200

    update

---
+++ 02b_OCI_demo_vcn_OL7_block_volumes_iscsi_remote_exec/01_auth.tf
@@ -0,0 +1,26 @@
+# ---- use variables defined in terraform.tfvars file
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "AD" {}
+variable "BootStrapFile_ol7" {}
+variable "ssh_public_key_file_ol7" {}
+variable "ssh_private_key_file_ol7" {}
+variable "authorized_ips" {}
+variable "cidr_vcn" {}
+variable "cidr_subnet1" {}
+variable "bkvol_attachment_name" {}
+variable "bkvol_mount_point" {}
+
+# ---- provider
+provider "oci" {
+  region           = var.region
+  tenancy_ocid     = var.tenancy_ocid
+  user_ocid        = var.user_ocid
+  fingerprint      = var.fingerprint
+  private_key_path = var.private_key_path
+}
+
