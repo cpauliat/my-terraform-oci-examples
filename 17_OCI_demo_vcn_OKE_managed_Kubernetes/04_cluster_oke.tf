4d12eb50d214565c10f6651da301985d97eef1c0
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2020-07-01 14:20:37 +0200

    update

--- 17_OCI_demo_vcn_OKE_managed_Kubernetes/04_cluster_oke.tf
+++ 17_OCI_demo_vcn_OKE_managed_Kubernetes/04_cluster_oke.tf
@@ -1,22 +1,3 @@
-# -------- get the list of available ADs
-data "oci_identity_availability_domains" "ADs" {
-  compartment_id = var.tenancy_ocid
-}
-
-# --------- Get the OCID for the most recent for Oracle Linux 7.x disk image
-data "oci_core_images" "ImageOCID-ol7" {
-  compartment_id           = var.compartment_ocid
-  operating_system         = "Oracle Linux"
-  operating_system_version = "7.7"
-
-  # filter to avoid Oracle Linux 7.x images for GPU
-  filter {
-    name   = "display_name"
-    values = ["^.*Oracle-Linux-7.7-[^G].*$"]
-    regex  = true
-  }
-}
-
 # --------- Create an OKE cluster
 resource "oci_containerengine_cluster" "tf-demo17-oke" {
   #Required
