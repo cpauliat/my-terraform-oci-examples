36536d043d660f079d3cef5652614a0292bc0d33
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2021-01-26 15:48:58 +0100

    update

--- 20_OCI_demo_autonomous_DB_serverless_SIMPLE/userdata/bootstrap_ol7.sh
+++ 20_OCI_demo_autonomous_DB_serverless_SIMPLE/userdata/bootstrap_ol7.sh
@@ -8,14 +8,14 @@ yum -y install oci-utils
 systemctl enable ocid.service
 systemctl start ocid.service
 
-echo "========== Install Oracle Instant client 18.3"
-yum install -y oracle-database-preinstall-18c.x86_64            # this creates oracle user
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-basic-18.3.0.0.0-3.x86_64.rpm
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-sqlplus-18.3.0.0.0-3.x86_64.rpm
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-tools-18.3.0.0.0-3.x86_64.rpm
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-devel-18.3.0.0.0-3.x86_64.rpm
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-jdbc-18.3.0.0.0-3.x86_64.rpm
-yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient18.3-odbc-18.3.0.0.0-3.x86_64.rpm
+echo "========== Install Oracle Instant client 19.3"
+yum install -y oracle-database-preinstall-19c.x86_64          # this creates oracle user
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-basic-19.3.0.0.0-1.x86_64.rpm
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-sqlplus-19.3.0.0.0-1.x86_64.rpm
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-tools-19.3.0.0.0-1.x86_64.rpm
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-devel-19.3.0.0.0-1.x86_64.rpm
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-jdbc-19.3.0.0.0-1.x86_64.rpm
+yum install -y http://yum.oracle.com/repo/OracleLinux/OL7/oracle/instantclient/x86_64/getPackage/oracle-instantclient19.3-odbc-19.3.0.0.0-1.x86_64.rpm
 
 echo "========== Create directory for ADB wallet"
 mkdir -p 700 /home/oracle/credentials_adb
@@ -28,13 +28,14 @@ chown -R oracle:dba /home/oracle/.ssh
 
 echo "========== Configure environment variables for user oracle"
 cat << EOF >> /home/oracle/.bash_profile
-PATH=$PATH:/usr/lib/oracle/18.3/client64/bin
+PATH=$PATH:/usr/lib/oracle/19.3/client64/bin
 LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/oracle/18.3/client64/lib
 TNS_ADMIN=/home/oracle/credentials_adb
 export PATH LD_LIBRARY_PATH TNS_ADMIN
-echo "Oracle Instant Client 18.3 installed"
+echo "Oracle Instant Client 19.3 installed"
 echo
 EOF
+chown oracle:dba /home/oracle/.bash_profile
 
 echo "========== Apply latest updates to Linux OS"
 yum update -y
