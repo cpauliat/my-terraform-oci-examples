45e60955f5413adeea2cbf32a6151725583ba886
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2021-06-01 18:20:33 +0200

    new file for data sources

--- 12c_OCI_demo_vcn_peering_local_DRG/13_instance_vcn1.tf
+++ 12c_OCI_demo_vcn_peering_local_DRG/13_instance_vcn1.tf
@@ -1,27 +1,13 @@
-# --------- Get the OCID for the most recent for Oracle Linux 7.x disk image
-data oci_core_images OLImageOCID-ol7-vcn1 {
-  compartment_id           = var.compartment_ocid
-  operating_system         = "Oracle Linux"
-  operating_system_version = "7.9"
-
-  # filter to avoid Oracle Linux 7.x images for GPU
-  filter {
-    name   = "display_name"
-    values = ["^.*Oracle-Linux-7.9-[^G].*$"]
-    regex  = true
-  }
-}
-
 # ------ Create a test compute instance (Oracle Linux 7) in VCN1
 resource oci_core_instance demo12c-vcn1 {
-  availability_domain = data.oci_identity_availability_domains.vcn1ADs.availability_domains[var.vcn1_instance_AD - 1]["name"]
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[var.vcn1_instance_AD - 1]["name"]
   compartment_id      = var.compartment_ocid
   display_name        = "demo12c-vcn1"
   shape               = "VM.Standard2.1"
 
   source_details {
     source_type = "image"
-    source_id   = data.oci_core_images.OLImageOCID-ol7-vcn1.images[0]["id"]
+    source_id   = data.oci_core_images.OLImageOCID-ol7.images[0]["id"]
   }
 
   create_vnic_details {
@@ -43,7 +29,7 @@ output Instance_VCN1 {
   ssh -i ${var.ssh_private_key_file} opc@${oci_core_instance.demo12c-vcn1.public_ip}
 
   ---- You can then ping instance in VCN2 using private IP address
-  ping ${oci_core_instance.demo12c-vcn2.private_ip}
+  ping ${oci_core_instance.demo12c-vcn2.private_ip}    
 
   ---- In this Terraform example, I also added the DNS private view for VCN2 to DNS resolver for VCN1
   ---- so, you can also ping instance in VCN2 using DNS hostname
--- 12c_OCI_demo_vcn_peering_local_DRG/14_instance_vcn2.tf
+++ 12c_OCI_demo_vcn_peering_local_DRG/14_instance_vcn2.tf
@@ -1,27 +1,13 @@
-# --------- Get the OCID for the most recent for Oracle Linux 7.x disk image
-data oci_core_images OLImageOCID-ol7-vcn2 {
-  compartment_id           = var.compartment_ocid
-  operating_system         = "Oracle Linux"
-  operating_system_version = "7.9"
-
-  # filter to avoid Oracle Linux 7.x images for GPU
-  filter {
-    name   = "display_name"
-    values = ["^.*Oracle-Linux-7.9-[^G].*$"]
-    regex  = true
-  }
-}
-
 # ------ Create a test compute instance (Oracle Linux 7) in VCN2
 resource oci_core_instance demo12c-vcn2 {
-  availability_domain = data.oci_identity_availability_domains.vcn2ADs.availability_domains[var.vcn2_instance_AD - 1]["name"]
+  availability_domain = data.oci_identity_availability_domains.ADs.availability_domains[var.vcn2_instance_AD - 1]["name"]
   compartment_id      = var.compartment_ocid
   display_name        = "demo12c-vcn2"
   shape               = "VM.Standard2.1"
 
   source_details {
     source_type = "image"
-    source_id   = data.oci_core_images.OLImageOCID-ol7-vcn2.images[0]["id"]
+    source_id   = data.oci_core_images.OLImageOCID-ol7.images[0]["id"]
   }
 
   create_vnic_details {
@@ -44,7 +30,7 @@ output Instance_VCN2 {
   ssh -i ${var.ssh_private_key_file} opc@${oci_core_instance.demo12c-vcn2.public_ip}
 
   ---- You can then ping instance in VCN1 using private IP address
-  ping ${oci_core_instance.demo12c-vcn1.private_ip}
+  ping ${oci_core_instance.demo12c-vcn1.private_ip}     
 
   ---- In this Terraform example, I also added the DNS private view for VCN1 to DNS resolver for VCN2
   ---- so, you can also ping instance in VCN1 using DNS hostname
