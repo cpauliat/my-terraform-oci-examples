901496a034a35bcdcdeb2a544c4ddb132e8d8bc2
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2021-09-27 15:32:41 +0200

    New demo16b

---
+++ 16b_OCI_demo_vcn_remote_state_bucket_http/01_variables.tf
@@ -0,0 +1,10 @@
+# ---- use variables defined in terraform.tfvars file
+variable "tenancy_ocid" {}
+variable "user_ocid" {}
+variable "fingerprint" {}
+variable "private_key_path" {}
+variable "compartment_ocid" {}
+variable "region" {}
+variable "authorized_ips" {}
+variable "cidr_vcn" {}
+variable "cidr_subnet1" {}
