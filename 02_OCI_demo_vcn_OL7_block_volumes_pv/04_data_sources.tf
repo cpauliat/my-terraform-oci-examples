8b788b13164ffbdcb177fec8fc8962de57f56a30
Christophe.pauliat@oracle.com <christophe.pauliat@oracle.com>
2022-09-13 15:35:48 +0200

    update demo02

--- 02_OCI_demo_vcn_OL7_block_volumes_pv/04_data_sources.tf
+++ 02_OCI_demo_vcn_OL7_block_volumes_pv/04_data_sources.tf
@@ -9,7 +9,7 @@ data oci_core_images ImageOCID-ol7 {
   operating_system         = "Oracle Linux"
   operating_system_version = "7.9"
 
-  # filter to keep only Oracle Linux 7.x images for GPU
+  # filter to keep only Oracle Linux 7.x images for x86-64 server (exclude GPU and ARM A1)
   filter {
     name   = "display_name"
     values = ["^.*Oracle-Linux-7.9-202.*$"]
